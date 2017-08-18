71a0d0430d84230f0bafd3abd670566d7e2511b4
jbtule <jay+code@tuley.name>
2017-08-18 14:23:51 -0500

    Demostrate core 2.0 dynamic keyword bug

--- DynamicKeywordComplexTypesBugCore2_0/UnitTest1.cs
+++ DynamicKeywordComplexTypesBugCore2_0/UnitTest1.cs
@@ -3,12 +3,32 @@ using Xunit;
 
 namespace DynamicKeywordComplexTypesBugCore2_0
 {
+    public class GenericType<T>
+    {
+        public class InnerClass
+        {
+            public string Go() => "Should Work!";
+        }
+    }
+
+
+
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+#if NETCOREAPP2_0
+        public void Test1_NETCOREAPP2_0()
+#elif NETCOREAPP1_1
+        public void Test1_NETCOREAPP1_1()
+#elif NETCOREAPP1_0
+        public void Test1_NETCOREAPP1_0()
+#else
+        public void Test1_NETCOREAPP_NETFX()
+#endif
         {
-
+            dynamic keyword = new GenericType<String>.InnerClass();
+            string result = keyword.Go();
+            Assert.Equal("Should Work!", result);
         }
     }
 }
