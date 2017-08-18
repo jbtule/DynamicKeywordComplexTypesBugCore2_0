8b0ac878db500b15d206635fc9582e56ddb40b31
jbtule <jay+code@tuley.name>
2017-08-18 13:35:49 -0500

    Add project files.

---
+++ DynamicKeywordComplexTypesBugCore2_0/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace DynamicKeywordComplexTypesBugCore2_0
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
